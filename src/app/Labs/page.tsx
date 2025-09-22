50ae091254806e7ee51aa6a69482d2c0d12a558f
kirankulkarni83 <kkulkarni8303@example.com>
2025-09-22 15:36:03 -0400

    WebDev Assignment 1

--- src/app/Labs/page.tsx
+++ src/app/Labs/page.tsx
@@ -3,6 +3,8 @@ export default function Labs() {
  return (
    <div id="wd-labs">
      <h1>Labs</h1>
+           <p id="wd-student-name">Kiran Ulhas Kulkarni</p>
+
      <ul>
        <li>
          <Link href="/Labs/Lab1" id="wd-lab1-link">
@@ -16,6 +18,11 @@ export default function Labs() {
          <Link href="/Labs/Lab3" id="wd-lab3-link">
            Lab 3: JavaScript Fundamentals </Link>
        </li>
+       <li>
+          <a href="https://github.com/kiranUlhasKulkarni/kambaz-next-js" id="wd-github" target="_blank" rel="noopener noreferrer">
+            Kambaz Source on GitHub
+          </a>
+        </li>
      </ul>
    </div>
 );}
