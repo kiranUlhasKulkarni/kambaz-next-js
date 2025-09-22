312a7733dafb092afd3dc0d7692f4c84e9fbcd8b
kirankulkarni83 <kkulkarni8303@example.com>
2025-09-22 15:08:57 -0400

    WebDev Assignment 1

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  eslint: {
+    // Ignore ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
