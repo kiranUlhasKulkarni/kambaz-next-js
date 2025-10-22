18c1434b73f12a6de7d4baa2f8b2a0e9a9b3379f
kirankulkarni83 <kkulkarni8303@example.com>
2025-10-21 21:16:32 -0400

    a3 JavaScript

--- src/app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
+++ src/app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
@@ -5,6 +5,9 @@ import { usePathname } from "next/navigation";
 
 export default function Breadcrumb({ course }: { course: { name: string } | undefined; }) {
  const pathname = usePathname();
+ const segments = pathname.split("/").filter(s => s);
+ const courseIndex = segments.indexOf("Courses");
+ const futureSegs = segments.slice(courseIndex + 2);
  return (
    <span>
      Course {course?.name} &gt; {pathname.split("/").pop()}
--- src/app/(Kambaz)/Courses/[cid]/layout.tsx
+++ src/app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,15 +1,18 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
+import * as db from "../../Database";
 import { FaAlignJustify } from "react-icons/fa";
-
+import Breadcrumb from "./Breadcrumb";
 export default async function CoursesLayout(
   { children, params }: Readonly<{ children: ReactNode; params: Promise<{ cid: string }> }>
 ) {
+  const courses = db.courses;
   const { cid } = await params;
+  const course = courses.find((c) => c._id === cid);
   return (
     <div id="wd-courses">
       <h2 className="text-danger">
-      <FaAlignJustify className="me-4 fs-4 mb-1" />Courses {cid}</h2>
+      <FaAlignJustify className="me-4 fs-4 mb-1" /><Breadcrumb course = {course}/></h2>
       <hr />
       <div className="d-flex">
       <div className="d-none d-md-block">
