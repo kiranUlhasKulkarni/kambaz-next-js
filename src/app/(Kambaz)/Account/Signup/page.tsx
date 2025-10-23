f51141fb97b156077cd7a5c065999ce145b9e229
kirankulkarni83 <kkulkarni8303@example.com>
2025-10-22 21:55:11 -0400

    a3 JavaScript

--- src/app/(Kambaz)/Account/Signup/page.tsx
+++ src/app/(Kambaz)/Account/Signup/page.tsx
@@ -4,11 +4,22 @@ export default function Signup() {
   return (
     <div id="wd-signup-screen">
       <h3>Sign up</h3>
-      <FormControl id="wd-username" placeholder="username" className="mb-2"/>
-      <FormControl id="wd-password" placeholder="password" type="password" className="mb-2"/> 
-      <FormControl id="wd-password-verify" placeholder="verify password" type="password" className="mb-2"/> 
-      <Link id="wd-signin-btn" href="Profile" className="btn btn-primary w-100 mb-2"> Sign up </Link>
-      <Link  href="Signin" > Sign in </Link>
+      <FormControl id="wd-username"
+             placeholder="username"
+             className="mb-2"/>
+      <FormControl id="wd-password"
+             placeholder="password" type="password"
+             className="mb-2"/>
+             <FormControl id="wd-verifypassword"
+             placeholder="password" type="password"
+             className="mb-2"/>
+              <Link id="wd-signin-btn"
+            href="/Account/Profile"
+            className="btn btn-primary w-100 mb-2">
+            Sign in </Link>
+            <Link id="wd-signup-btn"
+            href="/Account/Signup"
+            className="btn btn-primary w-100 mb-2">
+            Sign up </Link>
     </div>
 );}
-
