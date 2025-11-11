6695c753198de5bf6760313cf3ca5d2ccb0465a3
kiran kulkarni <118047637+kiranUlhasKulkarni@users.noreply.github.com>
2025-11-10 19:54:12 -0500

    Update page.tsx

--- src/app/(Kambaz)/Account/Signin/page.tsx
+++ src/app/(Kambaz)/Account/Signin/page.tsx
@@ -29,7 +29,7 @@ export default function Signin() {
       <FormControl defaultValue={credentials.password}
              onChange={(e) => setCredentials({ ...credentials, password: e.target.value })}
              id="wd-password" placeholder="password" type="password" className="mb-2"/> 
-      <Button onClick={signin} id="wd-signin-btn" className="w-100" > Sign in </Button>      
+      <Button onClick={signin} id="wd-signin-btn" className="w-100 mb-2" > Sign in </Button>      
       <Link id="wd-signup-btn" href="Signin" className="btn btn-primary w-100 mb-2" > Sign in </Link>
     </div>
 
